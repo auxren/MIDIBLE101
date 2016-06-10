5e416b5352464283670766133996629809e71892
Oren Levy <oren@auxren.com>
2016-06-09 19:49:19 -0700

    Added MIT license

--- MinimalMIDIBLE/MinimalMIDIBLE.ino
+++ MinimalMIDIBLE/MinimalMIDIBLE.ino
@@ -1,7 +1,29 @@
 /* Written by Oren Levy (auxren.com) based off some lackluster
- * documentation released by Intel.
- * MIDI over BLE info from: https://developer.apple.com/bluetooth/Apple-Bluetooth-Low-Energy-MIDI-Specification.pdf
- */
+   documentation released by Intel.
+   MIDI over BLE info from: https://developer.apple.com/bluetooth/Apple-Bluetooth-Low-Energy-MIDI-Specification.pdf
+  The MIT License (MIT)
+
+  Copyright (c) 2016 auxren
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
 #include <CurieBLE.h>
 
 #define TXRX_BUF_LEN              20 //max number of bytes
@@ -73,8 +95,8 @@ void BLESetup()
 void loop() {
 
   /*Simple randome note player to test MIDI output
-   * Plays random note every 400ms
-   */
+     Plays random note every 400ms
+  */
   int note = random(0, 127);
   //readMIDI();
   noteOn(0, note, 127); //loads up midiData buffer
