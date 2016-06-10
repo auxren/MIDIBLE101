7216efc248d83a58826d9a7813208f6210684a8e
Oren Levy <oren@auxren.com>
2016-06-09 19:48:58 -0700

    Repaired to work with Arduino 1.6.9. Added come comments. Added MIT license thing

--- Airpeggiator/Airpeggiator.ino
+++ Airpeggiator/Airpeggiator.ino
@@ -1,26 +1,56 @@
+/* Written by Oren Levy (auxren.com) based off some lackluster
+   documentation released by Intel and Jeff Rowberg's IMU code.
+   MIDI over BLE info from: https://developer.apple.com/bluetooth/Apple-Bluetooth-Low-Energy-MIDI-Specification.pdf
+
+
+  The MIT License (MIT)
+
+  Copyright (c) 2016 auxren
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  ===============================================
+*/
+
 //Set your tempo, key, and scale
 #define BPM        120
 #define MainKey    C
 #define MainScale  HarmonicMinor
 
-/* Written by Oren Levy (auxren.com) based off some lackluster
- * documentation released by Intel.
- * MIDI over BLE info from: https://developer.apple.com/bluetooth/Apple-Bluetooth-Low-Energy-MIDI-Specification.pdf
- */
-#include <BleCharacteristic.h>
-#include <BleDevice.h>
-#include <BleService.h>
-#include <BleCommon.h>
-#include <BleDescriptor.h>
-#include "CurieImu.h"
-#include <Time.h>
+#include <CurieBLE.h>
+#include "CurieIMU.h"
 
+//Setting up buffers for MIDI stuff
+#define TXRX_BUF_LEN              20 //max number of bytes
+#define RX_BUF_LEN                20 //max number of bytes
+uint8_t rx_buf[RX_BUF_LEN];
+int rx_buf_num, rx_state = 0;
+uint8_t rx_temp_buf[20];
+uint8_t outBufMidi[128];
+
+//Defining scales
 #define Major 0
 #define Minor 1
 #define Mixolydian 2
 #define MajorPentatonic 3
 #define MinorPentatonic 4
 #define HarmonicMinor 5
+//Defining notes
 #define C 0
 #define Db 1
 #define D 2
@@ -33,6 +63,7 @@
 #define A 9
 #define Bb 10
 #define B 11
+//Defining the actual scales
 uint8_t MajorScale[] = {0, 2, 4, 5, 7, 9, 11};
 uint8_t MinorScale[] = {0, 2, 3, 5, 7, 8, 10};
 uint8_t MixolydianScale[] = {0, 2, 4, 4, 7, 9, 10};
@@ -41,44 +72,20 @@ uint8_t MinorPentatonicScale[] = {0, 3, 5, 7, 10};
 uint8_t HarmonicMinorScale[] = {0, 2, 3, 5, 7, 8, 10};
 const float alpha = 0.5;
 
+//Variables for IMU stuff
 int rollNote = 0;
 int pitchNote = 0;
 int newNote = 0;
 int accelFlag = 0;
-
+int calibrateOffsets = 1; // int to determine whether calibration takes place or not
 double fXg = 0;
 double fYg = 0;
 double fZg = 0;
-
 double roll, pitch, yaw; // Roll and pitch are calculated using the accelerometer while yaw is calculated using the magnetometer
+int ax, ay, az;         // accelerometer values
+int gx, gy, gz;         // gyrometer values
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-#define TXRX_BUF_LEN                    20
-#define RX_BUF_LEN                      20
-uint8_t rx_buf[RX_BUF_LEN];
-int rx_buf_num, rx_state = 0;
-uint8_t rx_temp_buf[20];
-uint8_t outBufMidi[128];
-
-/* Bluetooth MAC address for this device */
-BleDeviceAddress localAddress;
-/* Bluetooth MAC address for remote peer device */
-BleDeviceAddress peerAddress;
-
-//Character ID for MIDI compliance
-uint8_t CHAR_UUID_MIDI[] = { 0xF3, 0x6B, 0x10, 0x9D, 0x66, 0xF2, 0xA9,
-
-                             0xA1, 0x12, 0x41, 0x68, 0x38, 0xDB, 0xE5, 0x72, 0x77
-                           };
-                           
-//Service ID for MIDI compliance
-uint8_t midiIOServiceUUID[] = { 0x00, 0xC7, 0xC4, 0x4E, 0xE3, 0x6C, 0x51,
-
-                                0xA7, 0x33, 0x4B, 0xE8, 0xED, 0x5A, 0x0E, 0xB8, 0x03
-                              };
-
+//Buffer to hold 5 bytes of MIDI data. Note the timestamp is forced
 uint8_t midiData[] = {0x80, 0x80, 0x00, 0x00, 0x00};
 
 //Loads up buffer with values for note On
@@ -97,93 +104,118 @@ void noteOff(char chan, char note) //channel 1
   midiData[4] = 0;
 }
 
-//Sets delay time for keeping the set tempo
-int BPMToMsDelay()
-{
-  int msOut = 600 / BPM;
-  return msOut;
+BLEPeripheral midiDevice; // create peripheral instance
+
+BLEService midiSvc("03B80E5A-EDE8-4B33-A751-6CE34EC4C700"); // create service
+
+// create switch characteristic and allow remote device to read and write
+BLECharacteristic midiChar("7772E5DB-3868-4112-A1A9-F2669D106BF3", BLEWrite | BLEWriteWithoutResponse | BLENotify | BLERead, 5);
+
+void setup() {
+  Serial.begin(9600);
+  IMUSetup();
+  BLESetup();
+  Serial.println(("Bluetooth device active, waiting for connections..."));
 }
-
-BleService midiSvc(midiIOServiceUUID);
-BleCharacteristic midiChar(CHAR_UUID_MIDI, 5, BLE_CLIENT_ACCESS_READ_WRITE, BLE_CLIENT_NOTIFY_ENABLED);
-BlePeripheral customDevice;
-
-/* Serial port to use for printing informational messages to the user */
-#define LOG_SERIAL Serial
-
-/* For convenience, this macro will invoke a specified function call and will
- * check the status value returned to ensure it is successful.  If not, it will
- * print an error message to the serial port and will return from the current function
- */
-#define CHECK_STATUS(op)                               \
-  do {                                                 \
-    BleStatus status = op;                             \
-    if (BLE_STATUS_SUCCESS != status) {                \
-      LOG_SERIAL.print(#op" returned error status: "); \
-      LOG_SERIAL.println(status);                      \
-      return;                                          \
-    }                                                  \
-  } while(0)
-
-void printBleDeviceAddress(BleDeviceAddress &address, const char *label)
-{
-  LOG_SERIAL.print(label);
-  LOG_SERIAL.print(" device address: ");
-
-  /* The address data is stored in little-endian format in memory so the
-   * bytes are printed in reverse-order to display a readable address */
-  for (int i = BLE_DEVICE_ADDR_LEN - 1; i >= 0 ; i--)
-    LOG_SERIAL.print(address.addr[i], HEX);
-
-  LOG_SERIAL.println();
-}
-/* This function will be called when a BLE GAP event is detected by the
- * Intel Curie BLE device */
-void blePeripheralEventCb(BlePeripheral &bleDevice, BlePeripheralEvent event, void *arg)
-{
-  
-}
-
-
 
 static void eventCallback(void)
 {
   accelFlag = 1;
 }
 
+void IMUSetup()
+{
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
 
+  // verify connection
+  Serial.println("Testing device connections...");
+  if (CurieIMU.begin()) {
+    Serial.println("CurieIMU connection successful");
+  } else {
+    Serial.println("CurieIMU connection failed");
+  }
 
-void setup() {
-  LOG_SERIAL.begin(115200);
-  Serial.begin(9600);
-  CurieImu.initialize();
-  CurieImu.attachInterrupt(eventCallback);
-  CurieImu.setIntTapEnabled(true);
-  CurieImu.setIntEnabled(true);
-  // Reduce threshold to allow detection of weaker taps (>= 750mg)
-  CurieImu.setTapDetectionThreshold(7); // (7 x 125mg)
-  CurieImu.autoCalibrateGyroOffset();
-  CurieImu.autoCalibrateXAccelOffset(0);
-  CurieImu.autoCalibrateYAccelOffset(0);
-  CurieImu.autoCalibrateZAccelOffset(1);
-  CurieImu.setGyroOffsetEnabled(true);
-  CurieImu.setAccelOffsetEnabled(true);
-  CurieImu.setAccelDLPFMode(2);
+  // use the code below to calibrate accel/gyro offset values
+  if (calibrateOffsets == 1) {
+    Serial.println("Internal sensor offsets BEFORE calibration...");
+    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
+    Serial.print("\t"); // -76
+    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
+    Serial.print("\t"); // -235
+    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
+    Serial.print("\t"); // 168
+    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
+    Serial.print("\t"); // 0
+    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
+    Serial.print("\t"); // 0
+    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
 
-  CHECK_STATUS(customDevice.setName("Airpeggiator")); //advertised device name
-  CHECK_STATUS(customDevice.init());
-  CHECK_STATUS(customDevice.getLocalAddress(localAddress));
-  //printBleDeviceAddress(localAddress, "local");
-  customDevice.setEventCallback(blePeripheralEventCb);
-  CHECK_STATUS(customDevice.addPrimaryService(midiSvc, true));
-  CHECK_STATUS(midiSvc.addCharacteristic(midiChar));
-  // CHECK_STATUS(midiChar.setValue(bleMIDIPacket1, 5));
-  CHECK_STATUS(customDevice.start());
+    Serial.println("About to calibrate. Make sure your board is stable and upright");
+    delay(5000);
 
-  LOG_SERIAL.println("Bluetooth device active, waiting for connections...");
+    // The board must be resting in a horizontal position for
+    // the following calibration procedure to work correctly!
+    Serial.print("Starting Gyroscope calibration and enabling offset compensation...");
+    CurieIMU.autoCalibrateGyroOffset();
+    Serial.println(" Done");
+
+    Serial.print("Starting Acceleration calibration and enabling offset compensation...");
+    CurieIMU.autoCalibrateAccelerometerOffset(X_AXIS, 0);
+    CurieIMU.autoCalibrateAccelerometerOffset(Y_AXIS, 0);
+    CurieIMU.autoCalibrateAccelerometerOffset(Z_AXIS, 1);
+    Serial.println(" Done");
+
+    Serial.println("Internal sensor offsets AFTER calibration...");
+    Serial.print(CurieIMU.getAccelerometerOffset(X_AXIS));
+    Serial.print("\t"); // -76
+    Serial.print(CurieIMU.getAccelerometerOffset(Y_AXIS));
+    Serial.print("\t"); // -2359
+    Serial.print(CurieIMU.getAccelerometerOffset(Z_AXIS));
+    Serial.print("\t"); // 1688
+    Serial.print(CurieIMU.getGyroOffset(X_AXIS));
+    Serial.print("\t"); // 0
+    Serial.print(CurieIMU.getGyroOffset(Y_AXIS));
+    Serial.print("\t"); // 0
+    Serial.println(CurieIMU.getGyroOffset(Z_AXIS));
+  }
 }
 
 
+void BLESetup()
+{
+  // set the local name peripheral advertises
+  midiDevice.setLocalName("Auxren");
+  midiDevice.setDeviceName("Auxren");
+
+  // set the UUID for the service this peripheral advertises
+  midiDevice.setAdvertisedServiceUuid(midiSvc.uuid());
+
+  // add service and characteristic
+  midiDevice.addAttribute(midiSvc);
+  midiDevice.addAttribute(midiChar);
+
+  // assign event handlers for connected, disconnected to peripheral
+  midiDevice.setEventHandler(BLEConnected, midiDeviceConnectHandler);
+  midiDevice.setEventHandler(BLEDisconnected, midiDeviceDisconnectHandler);
+
+  // assign event handlers for characteristic
+  midiChar.setEventHandler(BLEWritten, midiCharacteristicWritten);
+  // set an initial value for the characteristic
+  midiChar.setValue(midiData, 5);
+
+  // advertise the service
+  midiDevice.begin();
+}
+
+
+/* This quantizer takes in the unquantized not number,
+    the scale, and the key. It then figures out what note
+    to 'snap' to based off the scale definitions. This is probably
+    not the most efficient way to do this, but I needed something
+    quick and it works.
+*/
 uint8_t quantize(int note, char scale, char key)
 {
   int modNote = 0;
@@ -275,28 +307,42 @@ uint8_t quantize(int note, char scale, char key)
   }
 }
 
-
 void loop() {
-  CurieImu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // read raw accel/gyro measurements from device
+  CurieIMU.readMotionSensor(ax, ay, az, gx, gy, gz);
+  roll = atan2(ay, az) * RAD_TO_DEG; //calculate the roll
+  pitch = atan(-ax / sqrt(ay * ay + az * az)) * RAD_TO_DEG; //calculate the pitch
 
-  roll = atan2(ay, az) * RAD_TO_DEG;
-  pitch = atan(-ax / sqrt(ay * ay + az * az)) * RAD_TO_DEG;
-
-  rollNote = roll / 4 + 80;
-  pitchNote = pitch / 4 + 80;
+  rollNote = roll / 4 + 80; //convert the raw roll to a MIDI note value
+  pitchNote = pitch / 4 + 80; //convert the raw pitch to a MIDI note value
 
   if (pitch > 10) //filter out notes that are too low
   {
-    newNote = quantize(rollNote, MainScale, MainKey);
-    Serial.println(newNote);
-    noteOn(0, newNote, 127);
-    CHECK_STATUS(midiChar.setValue(midiData, 5));
-
+    newNote = quantize(rollNote, MainScale, MainKey); //Quantize the note
+    Serial.println(newNote); //print it out so we can see what it plays on the term
+    noteOn(0, newNote, 127); //turn the note on
+    midiChar.setValue(midiData, 5); //send that shit out over BLE
 
     delay(8);
-    noteOff(0, newNote);
-    CHECK_STATUS(midiChar.setValue(midiData, 5));
+    noteOff(0, newNote); //turn the note off
+    midiChar.setValue(midiData, 5); //send that shit out over BLE
   }
   delay(BPM);
 }
 
+void midiDeviceConnectHandler(BLECentral & central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+}
+
+void midiDeviceDisconnectHandler(BLECentral & central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+}
+
+void midiCharacteristicWritten(BLECentral & central, BLECharacteristic & characteristic) {
+  // central wrote new value to characteristic, update LED
+  Serial.print("Characteristic event, written: ");
+}
